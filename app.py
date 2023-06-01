95cf115da7a200e6729100041fc1bac2bbb12f70
aleona <ale.elsin92@gmail.com>
2023-06-01 21:23:53 +0200

    adding flasgger for improving api documentation

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
 from flask import Flask, jsonify
 from signal_transformer import get_demand_data
-from flask_restx import Api, Resource, Namespace
+from flasgger import Swagger
 
 app = Flask(__name__)
-api = Api(app, version="1.0", title="FFT Signal Decomposer", description="Single endpoint decomposing the Spanish Power Demand signal using Fast Fourier Transform.")
-fft_ns = Namespace("", description="Converts signal from its original domain (time) to a representation in the frequency domain.")
-api.add_namespace(fft_ns)
+swagger = Swagger(app)
 
-@fft_ns.route('/frequency-spectrum', methods=['GET'])
-class GetFrequency(Resource):
-    def get(self):
-        """Using data from 02/09/2018-06/10/2018"""
-        fft_results = get_demand_data()
-        if fft_results is not None:
-            return jsonify(fft_results)
-        else:
-            return jsonify({"error": "Error retrieving data"}), 500
+
+@app.route('/frequency-spectrum', methods=['GET'])
+def get_freq():
+    """
+    Converts spanish power demand signal data during 02/09/2018-06/10/2018 from its original domain (time) to a representation in the frequency domain.
+    ---
+    responses:
+      200:
+        description:  Frequency and amplitude values after applying FFT
+        schema:
+          type: object
+          properties:
+            amplitudes:
+              type: array
+              items:
+                type: number
+              example: [141883883, 575820.5569415133, 2064697.4945293446]
+            frequencies:
+              type: array
+              items:
+                type: number
+              example: [0, 0.00020420665713702266, 0.0004084133142740453]
+    """
+    fft_results = get_demand_data()
+    if fft_results is not None:
+        return jsonify(fft_results)
+    else:
+        return jsonify({"error": "Error retrieving data"}), 500
+
 
 if __name__ == '__main__':
     app.run()
-
-
-
 
